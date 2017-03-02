ea8ed6f569fc1b720900d4cbd9bf5294440c3903
jetsontwo <jetsontwo@gmail.com>
2017-03-01 17:16:52 -0800

    Allowed Players to Pick up and Drop Specified Items
    This will allow to players to pick up boxes and deliver them to the
    trucks or maybe in the future pick up workers to assign them to a new
    task.

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,6 +7,8 @@ public class Player_Movement : Movement{
 
     public float acceleration, drag, max_vel, vel_deadzone;
     private Rigidbody2D rb;
+    private GameObject holdable_object = null;
+    private bool holding = false;
 
     // Use this for initialization
     void Start()
@@ -20,6 +22,38 @@ public class Player_Movement : Movement{
         set_speed(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         move();
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (holding)
+                drop_item(holdable_object);
+            else if (holdable_object != null)
+                pickup_item(holdable_object);
+        }
+
+
     }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.CompareTag("Box"))
+            holdable_object = c.gameObject;
+        print("jasdfklj");
+    }
+
+    void pickup_item(GameObject obj)
+    {
+        obj.transform.parent = gameObject.transform;
+        obj.transform.position = new Vector3(gameObject.transform.position.x - 0.2f, gameObject.transform.position.y, 0);
+        holding = true;
+    }
+
+    void drop_item(GameObject obj)
+    {
+        obj.transform.parent = null;
+        holding = false;
+        holdable_object = null;
+    }
+
+
 
 }
