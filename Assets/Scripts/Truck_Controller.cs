3e634263aaef31e4eb5084a3a620202b3b8d6596
jetsontwo <jetsontwo@gmail.com>
2017-03-03 23:48:20 -0800

    Created Send Truck Script
    Sends the truck out and sells the boxes then it heads back.

--- Assets/Scripts/Truck_Controller.cs
+++ Assets/Scripts/Truck_Controller.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Truck_Controller : MonoBehaviour {
 
     public int spaces_x, spaces_y;
-    public float x_space, y_space;
+    public SpriteRenderer load_spot;
+    public float x_space, y_space, max_vel, accel, wait_time;
     private int cur_boxes_index = 0;
+    private GameObject[] boxes;
     private Vector2 init_location;
+    private Rigidbody2D rb;
+    private float target_x, original;
+
 
     void Start()
     {
         init_location = new Vector2(transform.position.x + 2.35f, transform.position.y + 0.5f);
+        boxes = new GameObject[spaces_x * spaces_y];
+        rb = GetComponent<Rigidbody2D>();
+        original = transform.position.x;
+        target_x = original + 20;
+
     }
 
     public void add_box(int size_x, int size_y, GameObject box)
@@ -26,6 +36,59 @@ public class Truck_Controller : MonoBehaviour {
             box.transform.position = new Vector2(init_location.x - (x_offset * x_space), init_location.y - (y_offset * y_space));
             cur_boxes_index++;
         }
+    }
 
+    public void send()
+    {
+        StartCoroutine(send_truck());
+    }
+
+    IEnumerator send_truck()
+    {
+        while (load_spot.color.a > 0)
+        {
+            load_spot.color = new Color(load_spot.color.r, load_spot.color.g, load_spot.color.b, load_spot.color.a - 0.02f);
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        while (transform.position.x < target_x)
+        {
+            if (rb.velocity.magnitude < max_vel)
+                rb.velocity += new Vector2(accel * Time.deltaTime, 0);
+            yield return new WaitForSeconds(0.1f);
+        }
+        Vector2 fast = rb.velocity;
+        rb.velocity = Vector2.zero;
+
+        yield return new WaitForSeconds(wait_time);
+
+        /////////////////////////Sell things here/////////////////////////////////////
+        
+        rb.velocity = -fast;
+        while (transform.position.x > original)
+        {
+            if(rb.velocity.magnitude > 0.1)
+                rb.velocity += new Vector2(accel/1.1f * Time.deltaTime, 0);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+        if (transform.position.x < original)
+            transform.position = new Vector3(original, transform.position.y, 0);
+        rb.velocity = Vector2.zero;
+
+        while (load_spot.color.a < 255)
+        {
+            load_spot.color = new Color(load_spot.color.r, load_spot.color.g, load_spot.color.b, load_spot.color.a + 0.02f);
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+
+
+    private void sell_boxes()
+    {
+        foreach(GameObject obj in boxes)
+        {
+
+        }
     }
 }
