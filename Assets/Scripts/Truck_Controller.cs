83d4d9501a24b0868e09875a7bb7c4464f79c2b8
jetsontwo <jetsontwo@gmail.com>
2017-03-03 19:29:17 -0800

    Added Art and Code for Truck
    Added top down view of truck with opaque veiw to show when boxes are
    inside and how full it is.  Also began code for truck to place boxes in
    itself.

---
+++ Assets/Scripts/Truck_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Truck_Controller : MonoBehaviour {
+
+    private GameObject[] boxes;
+    public int spaces_x, spaces_y;
+    private int cur_boxes_index = 0;
+
+    void Start()
+    {
+        boxes = new GameObject[spaces_x * spaces_y];
+    }
+
+    public void add_box(int size_x, int size_y, GameObject box)
+    {
+        boxes[cur_boxes_index] = box;
+        box.transform.parent = gameObject.transform;
+        box.transform.position = new Vector3();
+    }
+}
