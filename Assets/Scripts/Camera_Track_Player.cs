6a13237653385cd6ddff88d629440897dd467e79
jetsontwo <jetsontwo@gmail.com>
2017-03-03 20:42:12 -0800

    Camera Follow Script with Zoom
    Zooms based on scroll wheel and follows a given target (player for
    default).

---
+++ Assets/Scripts/Camera_Track_Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Track_Player : MonoBehaviour {
+
+    public GameObject thing_to_track;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(thing_to_track.transform.position.x, thing_to_track.transform.position.y, -10);
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < 5) // back
+            Camera.main.orthographicSize++;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > 2) // forward
+            Camera.main.orthographicSize--;
+        
+	}
+}
--- Assets/Scripts/Truck_Controller.cs
+++ Assets/Scripts/Truck_Controller.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Truck_Controller : MonoBehaviour {
 
-    private GameObject[] boxes;
     public int spaces_x, spaces_y;
     public float x_space, y_space;
     private int cur_boxes_index = 0;
@@ -12,20 +11,21 @@ public class Truck_Controller : MonoBehaviour {
 
     void Start()
     {
-        boxes = new GameObject[spaces_x * spaces_y];
         init_location = new Vector2(transform.position.x + 2.35f, transform.position.y + 0.5f);
     }
 
     public void add_box(int size_x, int size_y, GameObject box)
     {
-        boxes[cur_boxes_index] = box;
-        box.transform.parent = gameObject.transform;
+        if(cur_boxes_index < (spaces_x * spaces_y))
+        {
+            box.transform.parent = gameObject.transform;
 
-        int x_offset = cur_boxes_index / spaces_y;
-        int y_offset = cur_boxes_index % spaces_y;
+            int x_offset = cur_boxes_index / spaces_y;
+            int y_offset = cur_boxes_index % spaces_y;
 
-        print(x_offset + " " + y_offset);
-        box.transform.position = new Vector2(init_location.x - (x_offset * x_space), init_location.y - (y_offset * y_space));
-        cur_boxes_index++;
+            box.transform.position = new Vector2(init_location.x - (x_offset * x_space), init_location.y - (y_offset * y_space));
+            cur_boxes_index++;
+        }
+
     }
 }
