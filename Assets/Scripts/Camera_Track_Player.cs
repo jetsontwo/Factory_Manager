51ca7e58f7d3e98db223ec134eefb3dfd42e9f6f
jetsontwo <jetsontwo@gmail.com>
2017-03-03 22:57:15 -0800

    Added Front of Truck and Fixed Camera Tracker
    Made camera tracker stop at boundaries when fully zoomed in but created
    the front of truck to add more realism and allowed player to see outside
    their factory.

--- Assets/Scripts/Camera_Track_Player.cs
+++ Assets/Scripts/Camera_Track_Player.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class Camera_Track_Player : MonoBehaviour {
 
     public GameObject thing_to_track;
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(thing_to_track.transform.position.x, thing_to_track.transform.position.y, -10);
+    public float max_x, max_y;
+    void Start()
+    {
+        float vert_size = Camera.main.orthographicSize;
+        float horz_size = vert_size * Screen.width / Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 next_spot = new Vector3(thing_to_track.transform.position.x, thing_to_track.transform.position.y, -10);
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < 5) // back
             Camera.main.orthographicSize++;
         if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > 2) // forward
             Camera.main.orthographicSize--;
+
+        if(next_spot.x < max_x && next_spot.x > 0)
+            transform.position = new Vector3(next_spot.x, transform.position.y, -10);
+        if (next_spot.y > max_y && next_spot.y < 0)
+            transform.position = new Vector3(transform.position.x, next_spot.y, -10);
         
-	}
+
+    }
 }
