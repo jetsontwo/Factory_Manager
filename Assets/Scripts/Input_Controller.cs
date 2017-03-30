2715066efa081bc2fd4be91d80c6d040faa9d0a7
jetsontwo <jetsontwo@gmail.com>
2017-03-29 23:42:46 -0700

    Added Art for Iron and Tag
    Tag items allows player to tell the workers where a certain item should
    go.  It shows the art of the item as well as a color that can be changed
    for understanding and visual effect.

---
+++ Assets/Scripts/Input_Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Controller : MonoBehaviour {
+
+    //Used to hold all the keys for input for opening menus and other items before thoe features are implmented into UI elements or other things
+
+    public Buy_Menu bm;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Movement/Path_Creation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path_Creation : MonoBehaviour {
+
+    private 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
